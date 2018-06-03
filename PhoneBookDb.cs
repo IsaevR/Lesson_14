b435bbe3c8f33f5f6ceeeeb019738fe80b0a16a1
IsaevR <diplomatic.area@gmail.com>
2018-06-03 09:53:29 +0300

    firstcommit

---
+++ PhoneBookDb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookApp.DAO
+{
+    // Класс PhoneBookDb наследуется от DbContext
+    public class PhoneBookDb: DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        // Конструктор PhoneBookDb служит для обновления БД. Если данные в базе 
+        // не соответстуют входным данным, то база данных стирается и записывает входные данные.
+        // Это необходимо на уровне разработки для того что бы видеть изменения в базе данных.
+        // Когда проект пройдет тестирование и сдаетя в эксплуатацию следует удалить этот конструктор.
+        static PhoneBookDb()
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<PhoneBookDb>());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using PhoneBookApp.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person {Name = "Alexander", Phone = "986118792", Department = "Engineer" };
+            PhoneBookDb db = new PhoneBookDb();
+            db.People.Add(person);
+            db.SaveChanges();
+            Console.WriteLine("Id | Name       | Phone         |  Department  ");
+            Console.WriteLine("-----------------------------------------------");
+            foreach(var p in db.People)
+            {
+                Console.Write(" {0} | \b",p.Id);
+                Console.Write(" {0}",p.Name);
+                Console.Write("\t|{0}",p.Phone);
+                Console.Write("\t|{0}", p.Department);
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
